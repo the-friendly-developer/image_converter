95a61b3eaf31fe77c0bdddb95412d55b57417fd8
the-friendly-developer <230084984+the-friendly-developer@users.noreply.github.com>
2025-09-07 09:15:19 +0530

    Playing around

--- imageconverter.py
+++ imageconverter.py
@@ -1,5 +1,5 @@
 from PIL import Image
 import pillow_avif
 
-opnimg = Image.open('img1.avif')
+opnimg = Image.open('img1.avif') #converting avif to jpeg
 opnimg.convert('RGB').save("img_converted.jpg",'JPEG')
